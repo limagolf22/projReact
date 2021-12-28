d5aff6d49eb8d27d539405f11529213c1cff94e6
limagolf22 <70839782+limagolf22@users.noreply.github.com>
2021-12-28 19:28:32 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,40 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-import Testons from './components/testons';
-
 import QuestionsComponent from './components/questions';
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import openSocket from "socket.io-client";
+import { Socket } from 'socket.io-client';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Connexion from './components/Connexion';
 
 const ENDPOINT = "http://localhost:8080";
-const socket = openSocket(ENDPOINT, { transports: ['websocket'] });
-
-function RadioButton(){
-  return null;
-}
+const socket: Socket = openSocket(ENDPOINT, { transports: ['websocket'] });
+ 
+var gameOn = false;
 
 function App() {
   const [questions, setQuestions] = useState([]);
-
+  const [pseudos,setPseudos] = useState([]);
   useEffect(() => {
     socket.on("quiz", data => {
+      gameOn= true;
       setQuestions(data.quiz);
+      console.log("game started");
+      
     });
+    socket.on("users-changed", data => {
+      console.log(data);
+      setPseudos(data);
+  });
   }, []);
   return (
     
     <div className="App">
-               <Testons></Testons>
-
     <Container>
      <Row >
        <Col className="header">
@@ -41,7 +44,8 @@ function App() {
      </Row>
      </Container>
 
-     <QuestionsComponent questions={questions} socket={socket}></QuestionsComponent>
+      {gameOn? <QuestionsComponent questions={questions} socket={socket}/>:
+     <Connexion socket={socket} pseudos={pseudos} />}
      <Container>
        <Row>
        <footer id="site-footer">
